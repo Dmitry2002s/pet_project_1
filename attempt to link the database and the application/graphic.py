fd893ebc89f3a843b17b83930e79c11da8a5d15e
Дмитрий Ткаченко <st086996@student.spbu.ru>
2023-08-09 22:15:32 +0300

    stage 1

---
+++ attempt to link the database and the application/graphic.py
@@ -0,0 +1,29 @@
+﻿
+import PySimpleGUI as sg 
+import random
+from main import add,print_base 
+
+layout = [[sg.Button('Добавить задачу',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
+          [sg.Button('Отобразить базу данных',enable_events=True, key='-print-', font='Helvetica 16')], 
+        # затем делаем текст
+        [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
+
+# рисуем окно
+window = sg.Window('Task-manager', layout, size=(500,200))
+# запускаем основной бесконечный цикл
+while True:
+    # получаем события, произошедшие в окне
+    event, values = window.read()
+    # если нажали на крестик
+    if event == '-FUNCTION-' : 
+        add(sg.PopupGetText('Введите описание задачи'))
+    elif event == '-print-' : 
+        text = str()
+        for i in print_base() : 
+            text+= str(i['task_ID']) + '  ' + str(i['Description']) + '\n'
+        sg.PopupScrolled('Список задач' , text)
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        # выходим из цикла
+        break
+# закрываем окно и освобождаем используемые ресурсы
+window.close()
--- attempt to link the database and the application/main.py
+++ attempt to link the database and the application/main.py
@@ -2,48 +2,62 @@
 import pymysql 
 from config import host, user, password,db_name 
 
-try : 
-    connection  = pymysql.connect( 
-        host = host, 
-        port = 3306 ,
-        user = user ,
-        password = password, 
-        database= db_name, 
-        cursorclass= pymysql.cursors.DictCursor
-    )
-    print("succesfully connected...")
-    print("#" * 20)
 
-    
+def add(Description : str) : 
 
-    try  : 
-        with connection.cursor() as cursor : 
+    try : 
+        connection  = pymysql.connect( 
+            host = host, 
+            port = 3306 ,
+            user = user ,
+            password = password, 
+            database= db_name, 
+            cursorclass= pymysql.cursors.DictCursor
+        )
+        print("succesfully connected...")
+        print("#" * 20)
+        try  : 
+            with connection.cursor() as cursor : 
             
-            task_id = 0
-            cursor.execute("SELECT MAX(task_id) as maximum from task")
-            task_id = cursor.fetchall()
-            for i in task_id : 
-                task_id = int(i['maximum']) 
-            if(task_id == None ): 
-                task_id = 1 
-            else : 
-                task_id += 1 
-            Description = input('Enter your task \n') 
-            Person_ID = 1 
-            insert_query = "INSERT INTO task value(" + str(task_id) + ", '" +  Description + "' ,"  + str(Person_ID) + ")"
-            print(insert_query)
-            cursor.execute(insert_query)
-            connection.commit()
-            print('Insert succesfull')      
-
-            cursor.execute("select * from task") 
-            task_id = cursor.fetchall() 
-            for i in task_id : 
-                print(i)
-            
-
-    finally : 
-        connection.close()  
-except Exception as ex : 
-    print("connection refused...")
-    print("connection ex")
+                task_id = 0
+                cursor.execute("SELECT MAX(task_id) as maximum from task")
+                task_id = cursor.fetchall()
+                for i in task_id : 
+                    task_id = int(i['maximum']) 
+                if(task_id == None ): 
+                    task_id = 1 
+                else : 
+                    task_id += 1 
+                Person_ID = 1 
+                insert_query = "INSERT INTO task value(" + str(task_id) + ", '" +  Description + "' ,"  + str(Person_ID) + ")"
+                print(insert_query)
+                cursor.execute(insert_query)
+                connection.commit()
+                print('Insert succesfull')      
+        finally : 
+            connection.close()  
+    except Exception as ex : 
+        print("connection refused...")
+        print("connection ex")
+def print_base() : 
+    try : 
+        connection  = pymysql.connect( 
+            host = host, 
+            port = 3306 ,
+            user = user ,
+            password = password, 
+            database= db_name, 
+            cursorclass= pymysql.cursors.DictCursor
+        )
+        print("succesfully connected...")
+        print("#" * 20)
+        try  : 
+            with connection.cursor() as cursor :
+                cursor.execute("select * from task") 
+                task_id = cursor.fetchall() 
+        finally : 
+            connection.close()  
+            return task_id 
+    except Exception as ex : 
+        print("connection refused...")
+        print("connection ex")
