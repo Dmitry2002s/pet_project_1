e804fe88dbec2a19d802b4f8296027b5328c93eb
Дмитрий Ткаченко <st086996@student.spbu.ru>
2023-08-10 13:07:44 +0300

    Stage 2  _ registration/authentification added

--- attempt to link the database and the application/graphic.py
+++ attempt to link the database and the application/graphic.py
@@ -1,7 +1,10 @@
 ﻿
 import PySimpleGUI as sg 
 import random
-from main import add,print_base 
+from main import add,print_base, authentification 
+
+start_layout = [ [sg.Button('Аутентификация', enable_events=True, key = '-authentification-', font = 'Helvetica 16')], 
+                [sg.Button('Регистрация', enable_events=True, key = '-registration-', font = 'Helvetica 16')] ] 
 
 layout = [[sg.Button('Добавить задачу',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
           [sg.Button('Отобразить базу данных',enable_events=True, key='-print-', font='Helvetica 16')], 
@@ -10,19 +13,43 @@ layout = [[sg.Button('Добавить задачу',enable_events=True, key='-F
 
 # рисуем окно
 window = sg.Window('Task-manager', layout, size=(500,200))
+start_window = sg.Window('Task-manager', start_layout, size=(500,200))
+User_ID = 1
 # запускаем основной бесконечный цикл
-while True:
+Login_succes = True 
+while True :
     # получаем события, произошедшие в окне
+    event, values = start_window.read()
+    if event == '-authentification-' : 
+        nick = sg.PopupGetText('Введите логин ')
+        password  = sg.PopupGetText('Введите пароль ')
+        User_ID = authentification(nick, password , False )
+        if (User_ID == -2 ) : 
+            sg.PopupError('Логин/пароль не подходят')
+        elif (User_ID > -1 ) : 
+            sg.PopupError('Авторизация успешна')
+            break 
+    elif event == '-registration-' : 
+        nick = sg.PopupGetText('Введите логин создаваемого пользователя  ')
+        password  = sg.PopupGetText('Введите пароль создаваемого пользователя')
+        User_ID = authentification(nick, password, True )
+        if User_ID == -1 : 
+            print('Такой пользователь уже существует ')
+            sg.PopupError('Такой пользователь уже существует')
+    elif event in (sg.WIN_CLOSED, 'Exit'): # Выходим из цикла 
+        Login_succes = False 
+        break
+while Login_succes : 
     event, values = window.read()
     # если нажали на крестик
     if event == '-FUNCTION-' : 
-        add(sg.PopupGetText('Введите описание задачи'))
+        add(sg.PopupGetText('Введите описание задачи'), User_ID)
     elif event == '-print-' : 
         text = str()
-        for i in print_base() : 
+        for i in print_base(User_ID) : 
             text+= str(i['task_ID']) + '  ' + str(i['Description']) + '\n'
         sg.PopupScrolled('Список задач' , text)
-    if event in (sg.WIN_CLOSED, 'Exit'):
+    elif event in (sg.WIN_CLOSED, 'Exit'):
         # выходим из цикла
         break
 # закрываем окно и освобождаем используемые ресурсы
--- attempt to link the database and the application/main.py
+++ attempt to link the database and the application/main.py
@@ -1,9 +1,10 @@
-
+﻿
+from sqlite3 import Cursor
 import pymysql 
 from config import host, user, password,db_name 
 
 
-def add(Description : str) : 
+def add(Description : str, user_ID : int ) : 
 
     try : 
         connection  = pymysql.connect( 
@@ -28,8 +29,7 @@ def add(Description : str) :
                     task_id = 1 
                 else : 
                     task_id += 1 
-                Person_ID = 1 
-                insert_query = "INSERT INTO task value(" + str(task_id) + ", '" +  Description + "' ,"  + str(Person_ID) + ")"
+                insert_query = "INSERT INTO task value(" + str(task_id) + ", '" +  Description + "' ,"  + str(user_ID) + ")"
                 print(insert_query)
                 cursor.execute(insert_query)
                 connection.commit()
@@ -39,21 +39,21 @@ def add(Description : str) :
     except Exception as ex : 
         print("connection refused...")
         print("connection ex")
-def print_base() : 
+def print_base(user_ID : int ) : 
     try : 
         connection  = pymysql.connect( 
-            host = host, 
+            host = host , 
             port = 3306 ,
             user = user ,
             password = password, 
-            database= db_name, 
+            database = db_name, 
             cursorclass= pymysql.cursors.DictCursor
         )
         print("succesfully connected...")
         print("#" * 20)
         try  : 
             with connection.cursor() as cursor :
-                cursor.execute("select * from task") 
+                cursor.execute("select * from task where " + str(user_ID) + " = person_ID" ) 
                 task_id = cursor.fetchall() 
         finally : 
             connection.close()  
@@ -61,3 +61,52 @@ def print_base() :
     except Exception as ex : 
         print("connection refused...")
         print("connection ex")
+def authentification(nickname : str, user_password : str , registration : bool ) : #- отрицательный номер - в случае ошибки, положительный номер - ИД пользователя 
+    try : 
+        connection  = pymysql.connect( 
+            host = host , 
+            port = 3306 ,
+            user = user ,
+            password = password, 
+            database = db_name, 
+            cursorclass= pymysql.cursors.DictCursor
+        )
+        print ("Seccesfuly conncted") 
+        print ("#" * 20 ) 
+        if registration == True : 
+            try  : 
+                with connection.cursor() as cursor :
+                    cursor.execute("select Nickname from person")
+                    users = cursor.fetchall() 
+                    for i in users : 
+                        if i['Nickname'] == nickname : 
+                            result =  -1 # Сообщение об ошибке "Пользователь с таким именем уже существует
+                            return result
+                    cursor.execute("SELECT MAX(Person_ID) as maximum from person")
+                    person_ID = cursor.fetchall()[0]['maximum'] + 1 
+                    Request = "INSERT INTO person value ( " +str(person_ID) + ", '" + str(nickname) + "', '" + str(user_password) + "')"
+                    cursor.execute(Request  )
+                    connection.commit()
+                    print('Registration succesfull') 
+            except Exception as ex : 
+                print("registration failed") 
+            finally : 
+                connection.close()  
+        else : #Блок авторизации 
+            try : 
+                with connection.cursor() as cursor : 
+                    cursor.execute("select * from person") 
+                    users = cursor.fetchall() 
+                    for i in users : 
+                        if i['Nickname'] == nickname : 
+                            if i['password'] == user_password : 
+                                result = i['Person_ID']
+                                break 
+                        else : 
+                            result = -2 #Пользователь с таким именем не найден 
+            finally : 
+                connection.close()  
+                return result 
+    except Exception as ex : 
+            print ("connecion refused ") 
+            print ("connceion ex") 
