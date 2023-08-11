a95e48c533e9aaa70ee924ae0c66d434ec82a38e
Дмитрий Ткаченко <st086996@student.spbu.ru>
2023-08-11 12:26:20 +0300

    Stage 3 - added admin fuction

--- attempt to link the database and the application/graphic.py
+++ attempt to link the database and the application/graphic.py
@@ -1,20 +1,44 @@
 ﻿
 import PySimpleGUI as sg 
 import random
-from main import add,print_base, authentification 
+from main import add,print_base, authentification, delete_user, delete_task
+
+def UI_print(User_ID : int, all_users : bool ) : 
+    text = str()
+    None_base = print_base(User_ID, all_users)
+    if None_base != None : 
+        for i in None_base : 
+            text+= str(i['Person_ID']) + ' ' + str(i['task_ID']) + ' ' + str(i['Nickname']) + '  ' + str(i['Description']) + '\n'
+        sg.PopupScrolled('Список задач' , text)
+    else : 
+        sg.PopupScrolled('Список задач пуст!')
+
 
 start_layout = [ [sg.Button('Аутентификация', enable_events=True, key = '-authentification-', font = 'Helvetica 16')], 
                 [sg.Button('Регистрация', enable_events=True, key = '-registration-', font = 'Helvetica 16')] ] 
 
-layout = [[sg.Button('Добавить задачу',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
+layout = [[sg.Button('Добавить задачу',enable_events=True, key='-add_task-', font='Helvetica 16')],
           [sg.Button('Отобразить базу данных',enable_events=True, key='-print-', font='Helvetica 16')], 
         # затем делаем текст
         [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
 
+admin_layout = [ 
+               [sg.Button('Добавить задачу',enable_events=True, key='-add_task-', font='Helvetica 16')],
+               [sg.Button('Отобразить базу данных своих задач',enable_events=True, key='-print-', font='Helvetica 16')],
+               [sg.Button('Отобразить базу данных всех задач',enable_events=True, key='-print_admin-', font='Helvetica 16')],
+               [sg.Button('Удалить пользователя по ID', enable_events = True, key = '-delete_user-', font = 'Helvetca 16')],
+               [sg.Button('Удалить задачу по ID', enable_events = True, key = '-delete_task-', font = 'Helvetca 16')]
+]
+
+
+
+
 # рисуем окно
 window = sg.Window('Task-manager', layout, size=(500,200))
+admin_window = sg.Window('Admin Task-manager', admin_layout, size =(600, 300))
 start_window = sg.Window('Task-manager', start_layout, size=(500,200))
 User_ID = 1
+Admin = False 
 # запускаем основной бесконечный цикл
 Login_succes = True 
 while True :
@@ -26,8 +50,10 @@ while True :
         User_ID = authentification(nick, password , False )
         if (User_ID == -2 ) : 
             sg.PopupError('Логин/пароль не подходят')
-        elif (User_ID > -1 ) : 
+        elif (User_ID[0] > -1 ) : 
             sg.PopupError('Авторизация успешна')
+            Admin = User_ID[1] 
+            User_ID = User_ID[0]
             break 
     elif event == '-registration-' : 
         nick = sg.PopupGetText('Введите логин создаваемого пользователя  ')
@@ -39,18 +65,37 @@ while True :
     elif event in (sg.WIN_CLOSED, 'Exit'): # Выходим из цикла 
         Login_succes = False 
         break
-while Login_succes : 
-    event, values = window.read()
-    # если нажали на крестик
-    if event == '-FUNCTION-' : 
-        add(sg.PopupGetText('Введите описание задачи'), User_ID)
-    elif event == '-print-' : 
-        text = str()
-        for i in print_base(User_ID) : 
-            text+= str(i['task_ID']) + '  ' + str(i['Description']) + '\n'
-        sg.PopupScrolled('Список задач' , text)
-    elif event in (sg.WIN_CLOSED, 'Exit'):
-        # выходим из цикла
-        break
+if Admin == False : 
+    while Login_succes : 
+        event, values = window.read()
+        # если нажали на крестик
+        if event == '-add_task-' : 
+            add(sg.PopupGetText('Введите описание задачи'), User_ID)
+        elif event == '-print-' : 
+            UI_print(User_ID, False)
+        elif event in (sg.WIN_CLOSED, 'Exit'):
+            # выходим из цикла
+            break
+else : 
+    while Login_succes : 
+        event, values = admin_window.read() 
+        if event == '-add_task-' : 
+            add(sg.PopupGetText('Введите описание задачи'), User_ID)
+        elif event == '-print_admin-': 
+            UI_print(User_ID, True)
+        elif event == '-print-' : 
+            UI_print(User_ID, False)
+        elif event == '-delete_task-' :
+            task_ID = sg.PopupGetFile('Введите номер удаляемой задачи')
+            if (delete_task(task_ID ) == 1 ) : 
+                sg.PopupError("Удаление успешно!")
+            else : 
+                sg.PopupError("Ошибка!")
+        elif event == '-delete_user-' : 
+            delete_user(sg.PopupGetText('Введите номер удаляемого пользователя'))
+        elif event in (sg.WIN_CLOSED, 'Exit'):
+            # выходим из цикла
+            break
+
 # закрываем окно и освобождаем используемые ресурсы
 window.close()
--- attempt to link the database and the application/main.py
+++ attempt to link the database and the application/main.py
@@ -23,11 +23,12 @@ def add(Description : str, user_ID : int ) :
                 task_id = 0
                 cursor.execute("SELECT MAX(task_id) as maximum from task")
                 task_id = cursor.fetchall()
-                for i in task_id : 
-                    task_id = int(i['maximum']) 
-                if(task_id == None ): 
+                
+                if(task_id[0]['maximum'] == None ): 
                     task_id = 1 
                 else : 
+                    for i in task_id : 
+                        task_id = int(i['maximum']) 
                     task_id += 1 
                 insert_query = "INSERT INTO task value(" + str(task_id) + ", '" +  Description + "' ,"  + str(user_ID) + ")"
                 print(insert_query)
@@ -39,7 +40,8 @@ def add(Description : str, user_ID : int ) :
     except Exception as ex : 
         print("connection refused...")
         print("connection ex")
-def print_base(user_ID : int ) : 
+
+def print_base(user_ID : int , all : bool) : 
     try : 
         connection  = pymysql.connect( 
             host = host , 
@@ -53,7 +55,10 @@ def print_base(user_ID : int ) :
         print("#" * 20)
         try  : 
             with connection.cursor() as cursor :
-                cursor.execute("select * from task where " + str(user_ID) + " = person_ID" ) 
+                if (all == False) : 
+                    cursor.execute("select Person.Person_ID, Nickname,Description, task.task_ID from task, Person where " + str(user_ID) + " = task.person_ID and Person.Person_ID = task.Person_ID  " )
+                else : 
+                    cursor.execute("select Person.Person_ID, Nickname,Description, task.task_ID from task, Person where Person.Person_ID = task.Person_ID") 
                 task_id = cursor.fetchall() 
         finally : 
             connection.close()  
@@ -84,7 +89,7 @@ def authentification(nickname : str, user_password : str , registration : bool )
                             return result
                     cursor.execute("SELECT MAX(Person_ID) as maximum from person")
                     person_ID = cursor.fetchall()[0]['maximum'] + 1 
-                    Request = "INSERT INTO person value ( " +str(person_ID) + ", '" + str(nickname) + "', '" + str(user_password) + "')"
+                    Request = "INSERT INTO person value ( " +str(person_ID) + ", '" + str(nickname) + "', '" + str(user_password) + "', 0)"
                     cursor.execute(Request  )
                     connection.commit()
                     print('Registration succesfull') 
@@ -100,7 +105,7 @@ def authentification(nickname : str, user_password : str , registration : bool )
                     for i in users : 
                         if i['Nickname'] == nickname : 
                             if i['password'] == user_password : 
-                                result = i['Person_ID']
+                                result = [i['Person_ID'], i['ADMIN']]
                                 break 
                         else : 
                             result = -2 #Пользователь с таким именем не найден 
@@ -110,3 +115,52 @@ def authentification(nickname : str, user_password : str , registration : bool )
     except Exception as ex : 
             print ("connecion refused ") 
             print ("connceion ex") 
+def delete_user(User_ID) : 
+    try : 
+        connection  = pymysql.connect( 
+            host = host , 
+            port = 3306 ,
+            user = user ,
+            password = password, 
+            database = db_name, 
+            cursorclass= pymysql.cursors.DictCursor
+        )
+        print("succesfully connected...")
+        print("#" * 20)
+        try  : 
+            with connection.cursor() as cursor :
+                cursor.execute("delete from task where " + str(User_ID) + " = Person_ID") 
+                connection.commit()
+                cursor.execute("delete from Person where " + str(User_ID) + " = Person_ID") 
+                connection.commit()
+
+        finally : 
+            connection.close()  
+            return 
+    except Exception as ex : 
+        print("connection refused...")
+        print("connection ex")
+
+def delete_task(delete_Task_ID) : 
+    try : 
+        connection  = pymysql.connect( 
+            host = host , 
+            port = 3306 ,
+            user = user ,
+            password = password, 
+            database = db_name, 
+            cursorclass= pymysql.cursors.DictCursor
+        )
+        print("succesfully connected...")
+        print("#" * 20)
+        try  : 
+            with connection.cursor() as cursor :
+                cursor.execute("delete from task where " + str(delete_Task_ID) + " = Task_ID") 
+                connection.commit()
+        finally : 
+            connection.close()  
+            return 1
+    except Exception as ex : 
+        print("connection refused...")
+        print("connection ex")
+        return -1 
